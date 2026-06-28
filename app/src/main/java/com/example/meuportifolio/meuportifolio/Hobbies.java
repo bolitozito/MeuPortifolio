5e7471dcd840e850a4d0c49b5775ce4d4118806b
bolitozito <amendoinsanda@gmail.com>
2026-06-28 14:31:48 -0300

    Realização de Intents e correção de responsividade

--- app/src/main/java/com/example/meuportifolio/meuportifolio/Hobbies.java
+++ app/src/main/java/com/example/meuportifolio/meuportifolio/Hobbies.java
@@ -1,14 +1,37 @@
 package com.example.meuportifolio.meuportifolio;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Hobbies extends AppCompatActivity {
+
+    Intent nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hobbies);
+    }
+
+    public void clickVoltar (View view) {
+        finish();
+    }
+
+    public void clickGenshin(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://genshin-impact.fandom.com/wiki/Genshin_Impact_Wiki"));
+        startActivity(nav);
+    }
+
+    public void clickAlanzoka(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/@alanzoka/videos"));
+        startActivity(nav);
+    }
+
+    public void clickDormir(View view) {
+        nav = new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/MNYSYiuV8QA?si=8HL89piVQfS76U6g"));
+        startActivity(nav);
     }
 }
--- app/src/main/java/com/example/meuportifolio/meuportifolio/MainActivity.java
+++ app/src/main/java/com/example/meuportifolio/meuportifolio/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.meuportifolio.meuportifolio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent tela;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickSobre(View view) {
+        tela = new Intent(getApplicationContext(), SobreMim.class);
+        startActivity(tela);
+    }
+
+    public void clickHobbies(View view) {
+        tela = new Intent(getApplicationContext(), Hobbies.class);
+        startActivity(tela);
+    }
+
+    public void clickVideo(View view) {
+        tela = new Intent(getApplicationContext(), video.class);
+        startActivity(tela);
     }
 }
